5bf1a6c85671c5cb5c551fc6b70ed26538e73070
eCorbani <corbani.edson@gmail.com>
2023-12-07 16:55:04 -0500

    Table Event has been created, it is intended to work with the Schedule

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core.models import Event
+
 
 # Register your models here.
+
+class AdminEvent(admin.ModelAdmin):
+    list_display = ('title', 'event_date', 'creation_date')
+    list_filter = ('user', 'event_date')
+
+
+admin.site.register(Event, AdminEvent)
