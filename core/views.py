105557778ce7a82c0d42488616c5aa01fb77ff0e
eCorbani <corbani.edson@gmail.com>
2023-12-08 15:07:53 -0500

    Template to create events and gitgnore to ignore compiled files added

--- core/views.py
+++ core/views.py
@@ -39,3 +39,23 @@ def events_list(request):
     data = {'events': events}
 
     return render(request, 'schedule.html', data)
+
+
+@login_required(login_url='/login/')
+def event(request):
+    return render(request, 'event.html')
+
+
+@login_required(login_url='/login/')
+def submit_event(request):
+    if request.POST:
+        title = request.POST.get('title')
+        event_date = request.POST.get('event_date')
+        description = request.POST.get('description')
+        user = request.user
+        Event.objects.create(title=title,
+                             event_date=event_date,
+                             description=description,
+                             user=user)
+
+    return redirect('/')
