7e9ca8ab3f4b2231b174bd376faf9b614dcb9281
eCorbani <corbani.edson@gmail.com>
2023-12-08 12:01:36 -0500

    Created view to list events

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Event
+
 
 # Create your views here.
+
+# def index(request):
+#     return redirect('/schedule/')
+
+
+def events_list(request):
+    user = request.user
+    events = Event.objects.filter(user=user)
+    data = {'events': events}
+
+    return render(request, 'schedule.html', data)
